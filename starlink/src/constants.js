927d9bd8e50504cd28d0c8cd487c820e839dac5f
Yiwei Zhang <yiwei_zhang@ucsb.edu>
2022-08-19 22:48:29 -0700

    React JS based Starlink Trajectory Visualization

---
+++ starlink/src/constants.js
@@ -0,0 +1,11 @@
+export const SAT_API_KEY = "DJ58D7-DFMJMT-ZPC9J4-4VB7";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const BASE_URL = "https://54.90.198.36";;
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
